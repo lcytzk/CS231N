ad7bb5ec257719ab47042c9d72d7196bc523884b
lcytzk <lcytzk@github.com>
2017-06-17 19:10:59 +0800

    finish fully

--- assignment2/cs231n/optim.py
+++ assignment2/cs231n/optim.py
@@ -65,7 +65,9 @@ def sgd_momentum(w, dw, config=None):
   # TODO: Implement the momentum update formula. Store the updated value in   #
   # the next_w variable. You should also use and update the velocity v.       #
   #############################################################################
-  pass
+  #pass
+  v = config['momentum'] * v - config['learning_rate'] * dw
+  next_w =  w + v
   #############################################################################
   #                             END OF YOUR CODE                              #
   #############################################################################
@@ -92,6 +94,7 @@ def rmsprop(x, dx, config=None):
   config.setdefault('decay_rate', 0.99)
   config.setdefault('epsilon', 1e-8)
   config.setdefault('cache', np.zeros_like(x))
+  cache = config['cache']
 
   next_x = None
   #############################################################################
@@ -99,10 +102,13 @@ def rmsprop(x, dx, config=None):
   # in the next_x variable. Don't forget to update cache value stored in      #  
   # config['cache'].                                                          #
   #############################################################################
-  pass
+  #pass
+  cache = config['decay_rate'] * cache + (1 - config['decay_rate']) * dx ** 2
+  next_x = x - config['learning_rate'] * dx / (np.sqrt(cache) + config['epsilon'])
   #############################################################################
   #                             END OF YOUR CODE                              #
   #############################################################################
+  config['cache'] = cache
 
   return next_x, config
 
@@ -136,10 +142,20 @@ def adam(x, dx, config=None):
   # the next_x variable. Don't forget to update the m, v, and t variables     #
   # stored in config.                                                         #
   #############################################################################
-  pass
+  #pass
+  t = config['t'] + 1
+  m = config['beta1'] * config['m'] + (1-config['beta1']) * dx
+  v = config['beta2'] * config['v'] + (1-config['beta2']) * (dx ** 2)
+  m2 = m / (1 - config['beta1'] ** t)
+  v2 = v / (1 - config['beta2'] ** t)
+  next_x = x - config['learning_rate'] * m2 / (np.sqrt(v2) + config['epsilon'])
+  config['m'] = m
+  config['v'] = v
+  config['t'] = t
   #############################################################################
   #                             END OF YOUR CODE                              #
   #############################################################################
+  #config['t'] += 1
   
   return next_x, config
 
